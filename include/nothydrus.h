c2732771d5c5135302c89e31959eab8b60c7b59d
kreir42 <gortle97@gmail.com>
2024-05-23 12:18:18 +0200

    separate init function

---
+++ include/nothydrus.h
@@ -0,0 +1,13 @@
+#ifndef NOTHYDRUS_H
+#define NOTHYDRUS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "constants.h"
+
+void init();
+
+#endif
---
+++ src/init.c
@@ -0,0 +1,9 @@
+#include "nothydrus.h"
+
+void init(){
+	if(mkdir(INIT_DIRECTORY, DEFAULT_DIRECTORY_MODE)){
+		perror("Error when trying to create init directory");
+		return;
+	}
+	chdir(INIT_DIRECTORY);
+}
