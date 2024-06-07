d608f210e0a048a4926fb814d01d012fa3782798
kreir42 <gortle97@gmail.com>
2024-06-07 16:22:08 +0200

    taggroup_name_from_id util function

--- include/global_variables.h
+++ include/global_variables.h
@@ -19,6 +19,7 @@ extern sqlite3_stmt* add_taggroup_statement;
 extern sqlite3_stmt* tag_id_from_name_statement;
 extern sqlite3_stmt* taggroup_id_from_name_statement;
 extern sqlite3_stmt* tag_name_from_id_statement;
+extern sqlite3_stmt* taggroup_name_from_id_statement;
 extern sqlite3_stmt* tag_statement;
 extern sqlite3_stmt* untag_statement;
 
