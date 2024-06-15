bbd2a1ef575cb5b6c4d125afdb2abb73d5ac44bb
kreir42 <gortle97@gmail.com>
2024-06-15 17:28:44 +0200

    bugfix

--- src/tui.c
+++ src/tui.c
@@ -47,6 +47,9 @@ void start_tui(int_least8_t flags, void* data){
 	};
 	nc = notcurses_init(&opts, NULL);
 
+	struct timespec bugfix_timespec = {.tv_nsec = 100};
+	while(notcurses_get(nc, &bugfix_timespec, NULL)){}	//bugfix for notcurses getting strange input on startup; TBD find cause of weird input
+
 	if(flags & START_TUI_DISPLAY){
 		fullscreen_display((struct search*)data);
 		notcurses_drop_planes(nc);
