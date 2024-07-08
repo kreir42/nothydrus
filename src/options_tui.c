89c16c3e9b0f742d654d760cf107082e3d3d8954
kreir42 <gortle97@gmail.com>
2024-07-08 17:12:39 +0200

    can now also define default search order

--- src/options_tui.c
+++ src/options_tui.c
@@ -1,7 +1,7 @@
 #include "nothydrus.h"
 #include "tui.h"
 
-#define OPTIONS_TUI_MIN_ELEMENTS 2
+#define OPTIONS_TUI_MIN_ELEMENTS 3
 
 void options_tui(){
 	unsigned int screen_rows, screen_cols;
@@ -31,19 +31,29 @@ void options_tui(){
 				break;
 			case '+':
 				switch(ui_index){
-					case 0: tui_options.search_limit++; break;
+					case 1: tui_options.search_limit++; break;
 				}
 				break;
 			case '-':
 				switch(ui_index){
-					case 0: if(tui_options.search_limit>0) tui_options.search_limit--; break;
+					case 1: if(tui_options.search_limit>0) tui_options.search_limit--; break;
 				}
 				break;
 			case NCKEY_ENTER:
 				switch(ui_index){
 					case 0:
+						char* order_options[] = {"None", "Size", "Random", NULL};
+						tui_options.search_order_by = chooser(plane, order_options, tui_options.search_order_by);
+						if(tui_options.search_order_by==none || tui_options.search_order_by==random_order){
+							tui_options.search_descending = 0;
+						}else{
+							char* descending_options[] = {"Ascending", "Descending", NULL};
+							tui_options.search_descending = chooser(plane, descending_options, tui_options.search_descending);
+						}
 						break;
 					case 1:
+						break;
+					case 2:
 						break;
 				}
 				break;
@@ -54,7 +64,24 @@ void options_tui(){
 		//mark current index
 		if(ui_index<OPTIONS_TUI_MIN_ELEMENTS-1) ncplane_putstr_yx(plane, ui_index, 0, "->");
 		else ncplane_putstr_yx(plane, 2+ui_index, 0, "->");
-		ncplane_printf_yx(plane, 0, 3, "Default search limit (0 for none): %lu", tui_options.search_limit);	//limit
+		//order by
+		ncplane_putstr_yx(plane, 0, 3, "Default search order: ");
+		switch(tui_options.search_order_by){
+			case none:
+				ncplane_putstr(plane, "none");
+				break;
+			case size:
+				ncplane_putstr(plane, "size");
+				break;
+			case random_order:
+				ncplane_putstr(plane, "random");
+				break;
+		}
+		if(tui_options.search_order_by!=none && tui_options.search_order_by!=random_order){
+			if(tui_options.search_descending) ncplane_putstr(plane, " descending");
+			else ncplane_putstr(plane, " ascending");
+		}
+		ncplane_printf_yx(plane, 1, 3, "Default search limit (0 for none): %lu", tui_options.search_limit);	//limit
 		ncpile_render(plane);
 		ncpile_rasterize(plane);
 	}while((c=notcurses_get(nc, NULL, NULL))!='q');
