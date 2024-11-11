41315fe0f5c1afcf682bbdab48181c93465820d8
kreir42 <gortle97@gmail.com>
2024-11-11 08:09:44 +0100

    stderr now redirected to log file when using TUI

--- src/tui.c
+++ src/tui.c
@@ -231,7 +231,7 @@ void start_tui(int_least8_t flags, void* data){
 
 	struct timespec bugfix_timespec = {.tv_nsec = 100};
 	while(notcurses_get(nc, &bugfix_timespec, NULL)){}	//bugfix for notcurses getting strange input on startup; TBD find cause of weird input
-	freopen("/dev/null", "w", stderr);	//silence stderr
+	FILE* log_fp = freopen(INIT_DIRECTORY"/tui.log", "w", stderr);	//redirect stderr to log file
 
 	load_tui_options(INIT_DIRECTORY"/""tui_options");
 	if(flags & START_TUI_DISPLAY){
@@ -514,6 +514,7 @@ void start_tui(int_least8_t flags, void* data){
 	ncplane_destroy(search_plane);
 	notcurses_drop_planes(nc);
 	notcurses_stop(nc);
+	fclose(log_fp);
 }
 
 static sqlite3_int64 add_tag_to_file_tui(struct ncplane* parent_plane){
