92ff288008f4abb4dcc831f4777a01577c0fb944
kreir42 <gortle97@gmail.com>
2025-07-06 21:48:47 +0200

    fixed bug with external commands on filepaths with spaces

--- src/external_command.c
+++ src/external_command.c
@@ -7,9 +7,10 @@ void external_command_on_file(sqlite3_int64 id, char* command){
 		char* system_string = malloc((strlen(command)+strlen(filepath)+30)*sizeof(char));
 		system_string[0] = '\0';
 		strcat(system_string, command);
-		strcat(system_string, " ");
+		strcat(system_string, " \"");
 		strcat(system_string, filepath);
-		strcat(system_string, " 1>/dev/null 2>&1 0>&1");
+		strcat(system_string, "\" 1>/dev/null 2>&1 0>&1");
+		log_debug("Running external command: %s\n", system_string);
 		int status = system(system_string);	//TBD check status
 		free(system_string);
 	}
